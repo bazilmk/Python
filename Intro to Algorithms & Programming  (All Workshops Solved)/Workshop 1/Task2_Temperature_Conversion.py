81f310f609f34d33495493a8f6dc8f204cd70984
bazilmk <bazil.kotriwala@gmail.com>
2016-12-15 00:10:14 +0500

    Temperature Conversion from Fahrenheit to Celsius

---
+++ Intro to Algorithms & Programming  (All Workshops Solved)/Workshop 1/Task2_Temperature_Conversion.py
@@ -0,0 +1,7 @@
+''' Name: Bazil Muzaffar Kotriwala
+    Timestamp: 11:20pm 14 Dec - 2016
+    Farenheit to Celsius Program'''
+
+F = int(input('Give the temperature in Fahrenheit: '))
+C = (F - 32) * (5/9)
+print('The temperature is %s degrees Celsius' %C)
