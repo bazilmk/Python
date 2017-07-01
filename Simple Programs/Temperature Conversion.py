228adedd04184cf9bb04bfce1ce3e34ceda48c07
Bazil Muzaffar Kotriwala <bazil.kotriwala@gmail.com>
2017-07-01 21:07:22 +0500

    Update Temperature Conversion.py

--- Simple Programs/Temperature Conversion.py
+++ Simple Programs/Temperature Conversion.py
@@ -1,3 +1,7 @@
+''' Name: Bazil Muzaffar Kotriwala
+    Timestamp: 11:20pm 14 Dec - 2016
+    Farenheit to Celsius Program'''
+
 F = float(input('Enter the temperature in Farenheit: '))
 
 C = (F - 32) * (5/9)
